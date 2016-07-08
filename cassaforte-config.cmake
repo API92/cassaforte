867b86e043437a61b91788b2f53ceb8dc2d49e52
Andrey Pikas <API.92@ya.ru>
2016-07-08 19:21:10 +0300

    YouCompleteMe now uses compile_commands.json generated by CMake. Added cassaforte-config.cmake file.

---
+++ cassaforte-config.cmake
@@ -0,0 +1,26 @@
+# cassaforte-config
+# --------------
+#
+# This module defines
+#
+# ::
+#
+#   cassaforte_FOUND - Set to true when falloc is found.
+#   cassaforte_INCLUDE_DIR - the directories of the falloc headers
+#   cassaforte_LIBRARY - the falloc libraries needed for linking
+
+find_path(cassaforte_INCLUDE_DIR
+    NAME cass/defs.hpp
+    PATHS ${CMAKE_CURRENT_LIST_DIR}
+    PATH_SUFFIXES include)
+
+find_library(cassaforte_LIBRARY
+    NAMES cassaforte libcassaforte
+    PATHS ${CMAKE_CURRENT_LIST_DIR}
+    PATH_SUFFIXES bin/${CMAKE_BUILD_TYPE})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(cassaforte
+    "Could NOT find cassaforte, try to set the path to the cassaforte root folder in the variable cassaforte_DIR"
+    cassaforte_LIBRARY
+    cassaforte_INCLUDE_DIR)
